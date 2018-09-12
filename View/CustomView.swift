b8f99428e5ad2d67d56eea0a59088ce61863c64b
wurunfa <runfa.wu@ubtrobot.com>
2018-09-12 17:02:58 +0800

    添加了base和category代码

---
+++ View/CustomView.swift
@@ -0,0 +1,66 @@
+//
+//  CustomView.swift
+//  SwiftTest
+//
+//  Created by osmite on 2018/6/21.
+//  Copyright © 2018年 osmite. All rights reserved.
+//
+
+import UIKit
+
+public final class CustomView: UIView {
+    
+//    var aa :String?
+//    var bb :Int?
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        self.backgroundColor = UIColor.red
+////        let classA = ClassA()
+////        classA.text2()
+////        classA.text3()
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//
+//        self.backgroundColor = UIColor.red
+//    }
+    
+    
+    
+//    func printValue(_ firstArg:inout String,twoArg:inout String) -> Void {
+//        firstArg = "xxx"
+//        let attepStr = firstArg
+//        firstArg = twoArg
+//        twoArg = attepStr
+//        print("frisArg = \(firstArg) twoArg = \(twoArg)")
+//    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
+
+
+//class ClassA: UIView {
+//    private func text1(){
+//        print("text11")
+//    }
+//
+//    fileprivate func text2(){
+//        print("text22")
+//    }
+//}
+//
+//extension ClassA{
+//    func text3() -> Void {
+//        text1()
+//        text2()
+//    }
+//}
